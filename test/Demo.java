a147c5c6803addde75320eb42773316a7ea07918
qdzxout <qdzxout@outlook.com>
2022-11-10 15:18:46 +0800

    第一次提交

--- test/Demo.java
+++ test/Demo.java
@@ -1,7 +1,10 @@
 public class Demo{
     public static void main(String[] args){
     
-      System.out.println("这是一个测试")
+      System.out.println("这是一个测试");
+
+      System.out.println("12345678");
+
     }
 
 
